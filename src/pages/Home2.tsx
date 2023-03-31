e3b0dafcfe0c09d8436cee81d88129d2a4a1b5d5
CZandt <cole0hardy@gmail.com>
2023-03-31 11:24:43 -0600

    got the photo to show up :)

--- src/pages/Home2.tsx
+++ src/pages/Home2.tsx
@@ -1,7 +1,18 @@
+import joel from '../JoelHilton.jpg';
+
 function Home2() {
   return (
     <div>
-      <img src="../JoelHilton.jpg" alt="JoelHiltonHeadShot"></img>
+      <br />
+      <img
+        src={joel}
+        className="rounded mx-auto d-block"
+        alt="JoelHiltonHeadShot"
+      ></img>
+      <br />
+      <h1 className="text-center">
+        Welcome to the Joel Hilton Movie Collection
+      </h1>
     </div>
   );
 }
