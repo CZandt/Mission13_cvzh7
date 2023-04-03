bbb298364f5fac6b7dec1a30dbf967b0411afcb5
CZandt <cole0hardy@gmail.com>
2023-04-03 16:12:30 -0600

    intital branch

--- src/MovieCollection.tsx
+++ src/MovieCollection.tsx
@@ -1,9 +1,18 @@
 import { useState } from 'react';
 import data from './MovieData.json';
-
-const MovieData = data.MovieData;
+import { Movie } from './types/movie';
 
 function MovieCollection() {
+  const [movieData, setMovieData] = useState<Movie[]>([]);
+
+  const fetchMovie = async () => {
+    const rsp = await fetch('https://localhost:4000/movie');
+    const temp = await rsp.json();
+    setMovieData(temp);
+  };
+
+  fetchMovie();
+
   return (
     <>
       <br />
@@ -24,19 +33,23 @@ function MovieCollection() {
                     <th>Year</th>
                     <th>Rating</th>
                     <th>Category</th>
+                    <th>LentTo</th>
                     <th>Edited</th>
+                    <th>Notes</th>
                   </tr>
                 </thead>
 
                 <tbody>
-                  {MovieData.map((m) => (
+                  {movieData.map((m) => (
                     <tr>
-                      <td>{m.Title}</td>
-                      <td>{m.Director}</td>
-                      <td>{m.Year}</td>
-                      <td>{m.Rating}</td>
-                      <td>{m.Category}</td>
-                      <td>{m.Edited ? 'True' : true}</td>
+                      <td>{m.title}</td>
+                      <td>{m.director}</td>
+                      <td>{m.year}</td>
+                      <td>{m.rating}</td>
+                      <td>{m.category}</td>
+                      <td>{m.lentTo}</td>
+                      <td>{m.edited}</td>
+                      <td>{m.notes}</td>
                     </tr>
                   ))}
                 </tbody>
